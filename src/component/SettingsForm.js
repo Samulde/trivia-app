ebb2528a5e28805e7cde44f084693a9efed0bda1
samulde <samulde@gmail.com>
2021-10-21 19:36:50 +1100

    Restore to working state as I move to another project temporary

--- src/component/SettingsForm.js
+++ src/component/SettingsForm.js
@@ -1,92 +1,24 @@
-// import React from 'react';
-// import { Fragment } from 'react'
-// import { Menu, Transition } from '@headlessui/react'
-// import { ChevronDownIcon } from '@heroicons/react/solid'
-
-// const SettingForm = () => {
+import React from 'react';
 
 
+const SettingForm = () => {
 
-//   return (
-//     <Menu as="div" className="relative inline-block text-left">
-//       <div>
-//         <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-//           Options
-//           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-//         </Menu.Button>
-//       </div>
+  return (
+    <li class="group relative dropdown  px-4 text-purple-500 hover:text-purple-700 cursor-pointer font-bold text-base uppercase tracking-wide">
+      <a>Dropdown</a>
+      <div class="group-hover:block dropdown-menu absolute hidden h-auto">
+        <ul class="top-0 w-48 bg-white shadow px-6 py-8">
+          <li class="py-1"><a class="block text-purple-500 font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item</a></li>
+          <li class="py-1"><a class="block text-purple-500 font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 2</a></li>
+          <li class="py-1"><a class="block text-purple-500 font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 3</a></li>
+          <li class="py-1"><a class="block text-purple-500 font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 4</a></li>
+          <li class="py-1"><a class="block text-purple-500 font-bold text-base uppercase hover:text-purple-700 cursor-pointer">Item 5</a></li>
+       </ul>
 
-//       <Transition
-//         as={Fragment}
-//         enter="transition ease-out duration-100"
-//         enterFrom="transform opacity-0 scale-95"
-//         enterTo="transform opacity-100 scale-100"
-//         leave="transition ease-in duration-75"
-//         leaveFrom="transform opacity-100 scale-100"
-//         leaveTo="transform opacity-0 scale-95"
-//       >
-//         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-//           <div className="py-1">
-//             <Menu.Item>
-//               {({ active }) => (
-//                 <a
-//                   href="#"
-//                   className={classNames(
-//                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-//                     'block px-4 py-2 text-sm'
-//                   )}
-//                 >
-//                   Account settings
-//                 </a>
-//               )}
-//             </Menu.Item>
-//             <Menu.Item>
-//               {({ active }) => (
-//                 <a
-//                   href="#"
-//                   className={classNames(
-//                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-//                     'block px-4 py-2 text-sm'
-//                   )}
-//                 >
-//                   Support
-//                 </a>
-//               )}
-//             </Menu.Item>
-//             <Menu.Item>
-//               {({ active }) => (
-//                 <a
-//                   href="#"
-//                   className={classNames(
-//                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-//                     'block px-4 py-2 text-sm'
-//                   )}
-//                 >
-//                   License
-//                 </a>
-//               )}
-//             </Menu.Item>
-//             <form method="POST" action="#">
-//               <Menu.Item>
-//                 {({ active }) => (
-//                   <button
-//                     type="submit"
-//                     className={classNames(
-//                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-//                       'block w-full text-left px-4 py-2 text-sm'
-//                     )}
-//                   >
-//                     Sign out
-//                   </button>
-//                 )}
-//               </Menu.Item>
-//             </form>
-//           </div>
-//         </Menu.Items>
-//       </Transition>
-//     </Menu>
-//   )
+      </div>
 
-// }
+    </li>
+  )
+}
 
-// export default SettingForm;
+export default SettingForm;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   variants: {
     extend: {},
+    display:['group-hover']
   },
   plugins: [],
 }
